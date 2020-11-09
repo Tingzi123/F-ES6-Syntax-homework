9cc416642cfe07cf56171c72edac400fa21cae3e
Tingzi123 <1044373556@qq.com>
2020-11-09 23:59:38 +0800

    [ct]:feat test1 and test2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (input) => {
+    const { data, column } = input;
+    const keys = column.map(e => e.name);
+    return data.map(e=>{
+        const obj={}
+        keys.map((key,value)=>{
+            obj[key]=e[value]
+        })
+        return obj;
+    })
+  };
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) => {
+    let result  = [...items]
+    sections.forEach((section, index) => {
+        result.splice(section.index + index, 0, section.content)
+    })
+    return result
+  }
 export { inject };
